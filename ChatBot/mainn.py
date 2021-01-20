ad4367f48dcaf4cb26efca4a6164ad712303a5f4
Amartejas05 <54214228+Amartejas05@users.noreply.github.com>
2021-01-19 18:03:08 -0600

    Add files via upload

---
+++ ChatBot/mainn.py
@@ -0,0 +1,103 @@
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+nltk.download('punkt')
+
+import numpy
+import random
+import tflearn
+import json
+import tensorflow
+import pickle
+with open("intents.json") as file:
+    data = json.load(file)
+try:
+    with open("data.pickle", "rb") as f:
+        words,labels, training, output=pickle.load(f)
+except:
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
+
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds) #words
+            docs_y.append(intent["tag"]) #tags
+
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
+
+    words = [stemmer.stem(w.lower()) for w in words if w not in "?"]
+    words = sorted(list(set(words)))
+
+    labels = sorted(labels)
+
+    training = []
+    output = []
+
+    out_empty = [0 for _ in range(len(labels))]
+
+    for x, doc in enumerate(docs_x):
+        bag = []
+        wrds = [stemmer.stem(w) for w in doc]
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
+        out_row = out_empty[:]
+        out_row[labels.index(docs_y[x])] = 1
+
+        training.append(bag)
+        output.append(out_row)
+
+    training = numpy.array(training)
+    output = numpy.array(output)
+    with open("data.pickle", "wb") as f:
+        pickle.dump((words, labels, training, output), f)
+
+tensorflow.compat.v1.reset_default_graph()
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+try:
+    model.load("model.tflearn")
+except:
+    model.fit(training,output,n_epoch=1000, batch_size=8, show_metric=True)
+    model.save("model.tflearn")
+
+def bag_words(s, words):
+    bag = [0 for _ in range(len(words))]
+
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+    return numpy.array(bag)
+
+def chat():
+    print("Start talking to the bot. Type quit to exit")
+    while True:
+        inp = input("You:")
+        if inp == "quit":
+            break
+        result = model.predict([bag_words(inp, words)])
+        result_ind = numpy.argmax(result)
+        tag = labels[result_ind]
+
+        for tg in data["intents"]:
+            if tg["tag"] == tag:
+                response = tg["responses"]
+        print(random.choice(response))
+
+chat()
